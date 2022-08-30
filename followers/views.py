aee852e1b8d2f3cfebfce0fe145a54d8ce48f75c
EmanuelGustafzon <emanuel.gustafzon@gmail.com>
2022-08-30 15:11:18 +0000

    add followes api

--- followers/views.py
+++ followers/views.py
@@ -1,24 +1,24 @@
 from rest_framework import generics, permissions
 from drf_api.permissions import IsOwnerOrReadOnly
-from .models import Like
-from .serializer import LikeSerializer
+from .models import Follower
+from .serializer import FollowerSerializer
 
 
-class LikeList(generics.ListCreateAPIView):
+class FollowerList(generics.ListCreateAPIView):
     """
-    List Likes or like if logged in.
+    List followers or follow user if logged in.
     """
-    serializer_class = LikeSerializer
+    serializer_class = FollowerSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    queryset = Like.objects.all()
+    queryset = Follower.objects.all()
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
-class LikeDetail(generics.RetrieveDestroyAPIView):
+class FollowerDetail(generics.RetrieveDestroyAPIView):
     """
     Retrieve a like or delete it by id if you own it.
     """
     permission_classes = [IsOwnerOrReadOnly]
-    serializer_class = LikeSerializer
-    queryset = Like.objects.all()
+    serializer_class = FollowerSerializer
+    queryset = Follower.objects.all()
